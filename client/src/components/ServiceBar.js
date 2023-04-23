89632cd311b729dc89c6c7aecba7f4e71a824b70
Kreazy <102190042@sv1.dut.udn.vn>
2023-04-23 22:28:48 +0700

    fix: fix service bar

--- client/src/components/ServiceBar.js
+++ client/src/components/ServiceBar.js
@@ -1,29 +1,32 @@
 import React from "react"
 import './ServiceBar.css'
 
-const ServiceBar=()=>{
-    return(<>
-        <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
-    <aside class="app-sidebar">
-      <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="public/images/icons/img-1.jpg" width="30px" alt="User Image"/>
-        <div>
-          <p class="app-sidebar__user-designation">Welcome</p>
+const ServiceBar = () => {
+  return (<>
+    <div>
+      <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" />
+      <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+      <aside class="app-sidebar">
+        <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="public/images/icons/img-1.jpg" width="30px" alt="User Image" />
+          <div>
+            <p class="app-sidebar__user-designation">Welcome</p>
+          </div>
         </div>
-      </div>
 
-      <ul class="app-menu">
-        <li><a class="app-menu__item haha " ><i class='app-menu__icon bx bx-cart-alt'></i>
+        <ul class="app-menu">
+          <li><a class="app-menu__item haha " ><i class='app-menu__icon bx bx-cart-alt'></i>
             <span class="app-menu__label">Create Notifications</span></a></li>
-        <li><a class="app-menu__item  " ><i class='app-menu__icon bx bx-tachometer'></i><span
-              class="app-menu__label ">All Notification</span></a></li>
-        <li><a class="app-menu__item "><i class='app-menu__icon bx bx-id-card'></i>
+          <li><a class="app-menu__item  " ><i class='app-menu__icon bx bx-tachometer'></i><span
+            class="app-menu__label ">All Notification</span></a></li>
+          <li><a class="app-menu__item "><i class='app-menu__icon bx bx-id-card'></i>
             <span class="app-menu__label">Notification from producer </span></a></li>
-        <li><a class="app-menu__item " ><i class='app-menu__icon bx bx-user-voice'></i><span
-              class="app-menu__label">Notification from seller</span></a></li>
-        <li><a class="app-menu__item " ><i class='app-menu__icon bx bx-purchase-tag-alt'></i><span 
-              class="app-menu__label">Notification from insurance </span></a></li>
-      </ul>
-    </aside>
-    </>)
+          <li><a class="app-menu__item " ><i class='app-menu__icon bx bx-user-voice'></i><span
+            class="app-menu__label">Notification from seller</span></a></li>
+          <li><a class="app-menu__item " ><i class='app-menu__icon bx bx-purchase-tag-alt'></i><span
+            class="app-menu__label">Notification from insurance </span></a></li>
+        </ul>
+      </aside>
+    </div>
+  </>)
 }
 export default ServiceBar
